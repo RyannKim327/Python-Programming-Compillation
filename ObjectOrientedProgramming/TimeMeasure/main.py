48e55180d2919f962d26118d0358fee424a63003
RyannKim327 <rksesgundo123@gmail.com>
2023-01-31 20:43:39 +0800

    01-31-23 20:43

--- ObjectOrientedProgramming/TimeMeasure/main.py
+++ ObjectOrientedProgramming/TimeMeasure/main.py
@@ -7,5 +7,13 @@ def do():
 def do2():
 	print("Hello World")
 
-check(do)
-check(do2)
+def loop():
+	lists = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+	print(lists)
+	for i in range(len(lists)):
+		for j in range(i):
+			if lists[i] < lists[j]:
+				lists[i], lists[j] = lists[j], lists[i]
+	print(lists)
+
+check(loop)
