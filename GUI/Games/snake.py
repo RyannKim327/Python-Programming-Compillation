c4dc51b165cf02121a724d078fe682574776026e
RyannKim327 <rksesgundo123@gmail.com>
2023-03-01 13:20:48 +0800

    03-01-23 13:20 [Added Game over]

--- GUI/Games/snake.py
+++ GUI/Games/snake.py
@@ -1,5 +1,6 @@
 import pygame
-
+import time
+import random
 
 pygame.init()
 
@@ -12,6 +13,7 @@ game_over = False
 color_white = (255, 255, 255)
 color_green = (0, 255, 0)
 color_red = (255, 0, 0)
+color_black = (0, 0, 0)
 
 coordinate_x = 300
 coordinate_y = 300
@@ -20,6 +22,15 @@ x_change = 0
 y_change = 0
 
 clock = pygame.time.Clock()
+
+snake_block = 10
+snake_speed = 15
+
+font_style = pygame.font.SysFont(None, 50)
+
+def message(msg, color):
+	mess = font_style.render(msg, True, color)
+	display.blit(mess, [display.get_width() / 4, display.get_height() / 4])
 
 last_move = -1
 
@@ -45,6 +56,9 @@ while not game_over:
 				y_change -= 10
 				last_move = 3
 	
+	if coordinate_x >= display.get_width() or coordinate_y < 0 or coordinate_y >= display.get_height() or coordinate_y < 0:
+		game_over = True
+
 	coordinate_x += x_change
 	coordinate_y += y_change
 	display.fill(color_white)
@@ -52,6 +66,10 @@ while not game_over:
 	pygame.draw.rect(display, color_green, [coordinate_x, coordinate_y, 10, 10])
 	pygame.display.update()
 	clock.tick(30)
+
+message("Game Over", color_black)
+pygame.display.update()
+time.sleep(2)
 
 pygame.quit()
 quit()
