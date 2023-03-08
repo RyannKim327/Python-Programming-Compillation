d42ae4ac1cec6776b35e8019a445bc472bcdd710
RyannKim327 <rksesgundo123@gmail.com>
2023-03-08 20:02:05 +0800

    03-08-23 20:01

--- GUI/Games/maze.py
+++ GUI/Games/maze.py
@@ -6,6 +6,7 @@ c_white = (255, 255, 255)
 c_black = (0, 0, 0)
 c_blue = (0, 0, 255)
 c_green = (0, 150, 0)
+c_red = (100, 0, 0)
 
 block = 25
 movement = 25
@@ -18,8 +19,11 @@ pygame.display.set_caption("Maze game")
 
 ingame = True
 
-pos_x = 225 # display.get_width() // 2 - 5
-pos_y = 225 # display.get_height() // 2 - 5
+base_x = 225
+base_y = 225
+
+pos_x = base_x # display.get_width() // 2 - 5
+pos_y = base_y # display.get_height() // 2 - 5
 
 '''pattern = [
 	[pos_x, pos_y],
@@ -88,14 +92,14 @@ pos_y = 225 # display.get_height() // 2 - 5
 
 '''
   1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0
-1     *           * * * *               *
+1     *           * * *                 *
 2 *       * * *         * *   * * * * * *
 3 * * * * * * * * * *   * *             *
 4 * *                   * *   * * * *   *
-5 * *   * * * * * * * * * *   * *       *
-6 * *   *     * * * * * * *   * *   * * *
-7 *     *   *             *   * *   * * *
-8 *   *     *   * * * *   *   * *       *
+5 * *   * * * * * * * *   *   * *       *
+6 * *   *                 *   * *   * * *
+7 *     *   * * * * * * * *   * *   * * *
+8 *   *     *             *   * *       *
 9 *   *   * *   * * * *   *   * * * *   *
 0 *   *   * *   * * * *   *       * *   *
 1 *   *         *   * *   * * *   * *   *
@@ -117,10 +121,10 @@ xs = [
 	[2, 3, 4, 8, 9, 10, 11, 14],
 	[11, 14, 15, 16, 17, 18, 19],
 	[3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 19],
-	[3, 14, 17, 18, 19],
-	[3, 5, 6, 14, 17],
-	[2, 3, 5, 7, 8, 9, 10, 11, 12, 14, 17],
-	[2, 4, 5, 7, 12, 14, 17, 18, 19],
+	[3, 12, 14, 17, 18, 19],
+	[3, 5, 6, 7, 8, 9, 10, 11, 12, 14, 17],
+	[2, 3, 5, 14, 17],
+	[2, 4, 5, 7, 8, 9, 10, 11, 12, 14, 17, 18, 19],
 	[2, 4, 7, 12, 14, 19],
 	[2, 4, 7, 9, 10, 12, 14, 15, 16, 19],
 	[2, 4, 5, 6, 7, 9, 12, 16, 19],
@@ -178,9 +182,13 @@ while gaming:
 			pygame.draw.rect(display, c_white, [i[0], i[1], block, block])
 
 		pygame.draw.rect(display, c_green, [target[0], target[1], block, block])
+		pygame.draw.rect(display, c_red, [base_x, base_y, block, block])
 		
 		if pos_x == target[0] and pos_y == target[1]:
 			message("Congrats")
+
+		if pos_x == base_x and pos_y == base_y:
+			message("Start now")
 		
 		pygame.draw.rect(display, c_blue, [pos_x, pos_y, block, block])
 		pygame.display.update()
