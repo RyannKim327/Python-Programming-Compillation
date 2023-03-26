3d3da76cb5316224842ad2e4e01ce9bd82ea1617
RyannKim327 <rksesgundo123@gmail.com>
2023-03-27 06:46:21 +0800

    03-27-23 06:46

--- GUI/Notepad/main.py
+++ GUI/Notepad/main.py
@@ -1,10 +1,23 @@
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 
 def saveIt():
-	file = open(filename.get() + ".txt", "w")
-	file.write(text.get(1.0, "end-1c"))
-	messagebox.showinfo("Notice", "File Created/Updated Successfully")
+	if filename.get() == "":
+		filename_ = filedialog.askopenfilename(initialdir="/", title="Open a file", filetypes=(("Text Files", "*.txt"), ("All files", "*.*")))
+		file = open(filename_, "r")
+		file_ = filename_.split("/")[len(filename_.split("/")) - 1].split(".")[0]
+		text.insert(1.0, file.read())
+		filename.insert(0, file_)
+	else:
+		file = open(filename.get() + ".txt", "w")
+		file.write(text.get(1.0, "end-1c"))
+		messagebox.showinfo("Notice", "File Created/Updated Successfully")
+
+def check(event):
+	if event.widget.get() == "":
+		save.config(text="Open")
+	else:
+		save.config(text="Save")
 
 base = Tk()
 base.title("Notepad Application")
@@ -15,9 +28,10 @@ frame = Frame(base)
 
 filename = Entry(frame)
 filename.config(width=65)
+filename.bind("<FocusOut>", check)
 filename.pack(side='left')
 
-save = Button(frame, text="Save")
+save = Button(frame, text="Open")
 save.config(command=saveIt)
 save.pack(fill=X)
 
