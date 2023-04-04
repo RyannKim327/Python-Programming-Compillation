9482356441c095fb9fd359fa33b7a858c492fd9b
RyannKim327 <rksesgundo123@gmail.com>
2023-04-04 08:24:36 +0800

    04-04-23 08:24

--- OpenCV Projects/Detector/main.py
+++ OpenCV Projects/Detector/main.py
@@ -5,6 +5,16 @@ img = cv2.imread("a.png")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_rbg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+xml_data = cv2.CascadeClassifier("XML-Data.xml") # I don't know where do I need to get this
+detect = xml_data.detectMultiScale(img_gray, minSize = (30, 30))
+amt_det = len(detect)
+
+if amt_det !=0:
+	for(a, b, width, height) in detect:
+		cv2.rectangle(img_rbg, (a, b),
+		(a + height, b + width),
+		(0, 275, 0), 9)
+
 plt.subplot(1, 1, 1)
 plt.imshow(img_rbg)
 plt.show()
