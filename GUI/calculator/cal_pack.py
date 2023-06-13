8330c7e3d0e5b5f773f06dbaf4046629bc9e4f1a
Mr.RySes <mpoprev2@email.com>
2023-06-13 18:35:48 +0800

    06-13-23 18:36

--- GUI/calculator/cal_pack.py
+++ GUI/calculator/cal_pack.py
@@ -8,6 +8,7 @@ def start():
 	root.geometry('300x300')
 	root.title('Calculator')
 	root.config(bg="#131320")
+	root.resizable(False, False)
 
 	wala_kaming = Label(root, text="Calculator", bg="#131320", fg='#ffffff', font=('Times New Roman', 25))
 	wala_kaming.pack(fill='x')
@@ -15,63 +16,72 @@ def start():
 	e = Entry(root, bg='#131320', fg='#ffffff', font=('Courier New', 25), justify='right')
 	e.pack(fill='x', padx=5, pady=5)
 
+	be = Button(root, bg='#131320', fg='#ffffff', text='=', width=5, height=10)
+	be.pack(side='right', anchor='n', padx=pads, pady=pads)
+
 	row1 = Frame(root, bg='#131320')
 
 	b1 = Button(row1, bg='#131320', fg='#ffffff', text='1', width=5)
-	b1.pack(side='left', padx=pads, pady=pads)
+	b1.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	b2 = Button(row1, bg='#131320', fg='#ffffff', text='2', width=5)
-	b2.pack(side='left', padx=pads, pady=pads)
+	b2.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	b3 = Button(row1, bg='#131320', fg='#ffffff', text='3', width=5)
-	b3.pack(side='left', padx=pads, pady=pads)
-
-	b4 = Button(row1, bg='#131320', fg='#ffffff', text='4', width=5)
-	b4.pack(side='left', padx=pads, pady=pads)
+	b3.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	ba = Button(row1, bg='#131320', fg='#ffffff', text='+', width=5)
-	ba.pack(side='left', padx=pads, pady=pads)
-
+	ba.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	row1.pack(fill='x')
 	
 	row2 = Frame(root, bg='#131320')
 
+	b4 = Button(row2, bg='#131320', fg='#ffffff', text='4', width=5)
+	b4.pack(side='left', anchor='n', padx=pads, pady=pads)
+
 	b5 = Button(row2, bg='#131320', fg='#ffffff', text='5', width=5)
-	b5.pack(side='left', padx=pads, pady=pads)
+	b5.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	b6 = Button(row2, bg='#131320', fg='#ffffff', text='6', width=5)
-	b6.pack(side='left', padx=pads, pady=pads)
-
-	b7 = Button(row2, bg='#131320', fg='#ffffff', text='7', width=5)
-	b7.pack(side='left', padx=pads, pady=pads)
-
-	b8 = Button(row2, bg='#131320', fg='#ffffff', text='8', width=5)
-	b8.pack(side='left', padx=pads, pady=pads)
+	b6.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	bs = Button(row2, bg='#131320', fg='#ffffff', text='-', width=5)
-	bs.pack(side='left', padx=pads, pady=pads)
+	bs.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	row2.pack(fill='x')
 
 	row3 = Frame(root, bg='#131320')
 
-	b9 = Button(row3, bg='#131320', fg='#ffffff', text='9', width=5)
-	b9.pack(side='left', padx=pads, pady=pads)
+	b7 = Button(row3, bg='#131320', fg='#ffffff', text='7', width=5)
+	b7.pack(side='left', anchor='n', padx=pads, pady=pads)
 
-	b0 = Button(row3, bg='#131320', fg='#ffffff', text='0', width=5)
-	b0.pack(side='left', padx=pads, pady=pads)
+	b8 = Button(row3, bg='#131320', fg='#ffffff', text='8', width=5)
+	b8.pack(side='left', anchor='n', padx=pads, pady=pads)
+
+	b9 = Button(row3, bg='#131320', fg='#ffffff', text='9', width=5)
+	b9.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	bm = Button(row3, bg='#131320', fg='#ffffff', text='*', width=5)
-	bm.pack(side='left', padx=pads, pady=pads)
-
-	bq = Button(row3, bg='#131320', fg='#ffffff', text='/', width=5)
-	bq.pack(side='left', padx=pads, pady=pads)
-
-	be = Button(row3, bg='#131320', fg='#ffffff', text='=', width=5)
-	be.pack(side='left', padx=pads, pady=pads)
+	bm.pack(side='left', anchor='n', padx=pads, pady=pads)
 
 	row3.pack(fill='x')
+
+	row4 = Frame(root, bg='#131320')
+
+	bq = Button(row4, bg='#131320', fg='#ffffff', text='/', width=5)
+	bq.pack(side='left', anchor='n', padx=pads, pady=pads)
+
+	b0 = Button(row4, bg='#131320', fg='#ffffff', text='0', width=5)
+	b0.pack(side='left', anchor='n', padx=pads, pady=pads)
+	
+	bd = Button(row4, bg='#131320', fg='#ffffff', text='.', width=5)
+	bd.pack(side='left', anchor='n', padx=pads, pady=pads)
+	
+	bc = Button(row4, bg='#131320', fg='#ffffff', text='CE', width=5)
+	bc.pack(side='left', anchor='n', padx=pads, pady=pads)
+
+	row4.pack(fill='x')
 
 	root.mainloop()
 
