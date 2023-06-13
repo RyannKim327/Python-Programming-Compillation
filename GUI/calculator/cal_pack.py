28c72c13cdc4a57fe1e8750bb60d19f7fe205dad
Mr.RySes <mpoprev2@email.com>
2023-06-13 14:28:10 +0800

    06-13-23 14:28

--- GUI/calculator/cal_pack.py
+++ GUI/calculator/cal_pack.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 def start():
 
+	pads = 8
+
 	root = Tk()
 	root.geometry('300x300')
 	root.title('Calculator')
@@ -10,36 +12,35 @@ def start():
 	e = Entry(root, bg='#131320', fg='#ffffff')
 	e.pack(fill='x', padx=5, pady=5)
 
-	b1 = Button(root, text="1", width=5, height=2)
-	b1.pack(side='left', anchor='n', padx=5, pady=5)
+	b1 = Button(root, bg='#131320', fg='#ffffff', text='1', width=5)
+	b1.pack(side='left', anchor='n', padx=pads, pady=pads)
 
-	b2 = Button(root, text="2", width=5, height=2)
-	b2.pack(side='left', anchor='n', padx=5, pady=5)
-	
-	b3 = Button(root, text="3", width=5, height=2)
-	b3.pack(side='left', anchor='n', padx=5, pady=5)
-	
-	b4 = Button(root, text="4", width=5, height=2)
-	b4.pack(side='left', anchor='n', padx=5, pady=5)
-	
-	b5 = Button(root, text="5", width=5, height=2)
-	b5.pack(side='left', anchor='n', padx=5, pady=5)
+	b2 = Button(root, bg='#131320', fg='#ffffff', text='2', width=5)
+	b2.pack(fill='x', side='left', anchor='n', padx=pads, pady=pads)
 
-	b6 = Button(root, text="6", width=5, height=2)
-	b6.pack(side='left', padx=5, pady=5)
+	b3 = Button(root, bg='#131320', fg='#ffffff', text='3', width=5)
+	b3.pack(fill='x', side='left', anchor='n', padx=pads, pady=pads)
 
-	b7 = Button(root, text="7", width=5, height=2)
-	b7.pack(side='left', padx=5, pady=5)
-	
-	b8 = Button(root, text="8", width=5, height=2)
-	b8.pack(side='left', padx=5, pady=5)
-	
-	b9 = Button(root, text="9", width=5, height=2)
-	b9.pack(side='left', padx=5, pady=5)
-	
-	b0 = Button(root, text="0", width=5, height=2)
-	b0.pack(side='left', padx=5, pady=5)
+	b4 = Button(root, bg='#131320', fg='#ffffff', text='4', width=5)
+	b4.pack(fill='x', side='left', anchor='n', padx=pads, pady=pads)
 
+	b5 = Button(root, bg='#131320', fg='#ffffff', text='5', width=5)
+	b5.pack(fill='x', side='left', anchor='n', padx=pads, pady=pads)
+
+	b6 = Button(root, bg='#131320', fg='#ffffff', text='1', width=5)
+	b6.pack(fill='x', side='bottom', padx=pads, pady=pads)
+
+	b7 = Button(root, bg='#131320', fg='#ffffff', text='2', width=5)
+	b7.pack(fill='x', side='left', anchor='n', padx=pads, pady=pads)
+
+	b8 = Button(root, bg='#131320', fg='#ffffff', text='3', width=5)
+	b8.pack(fill='x', side='left', anchor='n', padx=pads, pady=pads)
+
+	b9 = Button(root, bg='#131320', fg='#ffffff', text='4', width=5)
+	b9.pack(fill='x', side='left', anchor='n', padx=pads, pady=pads)
+
+	b0 = Button(root, bg='#131320', fg='#ffffff', text='5', width=5)
+	b0.pack(fill='x', side='left', anchor='n', padx=pads, pady=pads)
 
 	root.mainloop()
 
