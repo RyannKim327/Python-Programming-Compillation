3ca15dc346e9866ce0eeb4bf3eddfbdfef7a6030
Mr.RySes <mpoprev2@email.com>
2023-06-17 19:15:12 +0800

    06-17-23 19:15

--- GUI/registration/registration.py
+++ GUI/registration/registration.py
@@ -1,11 +1,57 @@
 from tkinter import *
 from tkinter import ttk
+
+import os
+
+import openpyxl as xl
 import datetime
+		
+def createDatabase(data: list):
+	global xls, wb
+
+	if os.path.exists("book.xlsx"):
+		wb = xl.load_workbook("book.xlsx")
+		xls = wb['currentdatabase']
+	else:
+		wb = xl.Workbook()
+		xls = wb.active
+		xls.title = "currentdatabase"
+	
+	xls.cell(row = 1, column = 1, value = "ID")
+	for i in range(0, len(data)):
+		xls.cell(row = 1, column = i + 2, value = data[i])
+
+def addData(data: list):
+	rows = xls.max_row + 1
+	xls.cell(row = rows, column = 1, value = rows - 1)
+	for i in range(0, len(data)):
+		xls.cell(row = rows, column = i + 2, value = data[i])
+	
+	wb.save("book.xlsx")
 
 def execute():
-	print(str_sex.get())
+	"Name:        "
+	"Age:         "
+	"Sex:         "
+	"Course:      "
+	"Year:        "
+	"Address:     "
+	"Phone:       "
+	"Description: "
+	data = []
+	data.append(name_entry.get())
+	data.append(age_entry.get())
+	data.append(str_sex.get())
+	course = ""
 	for x in course_lists:
-		print(x.get())
+		course += x.get() + ", "
+	data.append(course)
+	data.append(year_entry.get())
+	data.append(addr_entry.get())
+	data.append(phone_entry.get())
+	data.append(desc_entry.get("1.0", "end-1c"))
+
+	addData(data)
 
 def changeAge():
 	if year_entry.get().isdigit():
@@ -164,4 +210,15 @@ def start():
 	root.mainloop()
 
 if __name__ == "__main__":
+	data = [
+		"name",
+		"age",
+		"sex",
+		"course",
+		"year",
+		"address",
+		"phone",
+		"description"
+	]
+	createDatabase(data)
 	start()
