c69037201f6311c26aa231ea6e55de320dad4cdf
Mr.RySes <mpoprev2@email.com>
2023-07-01 23:30:28 +0800

    07-01-23 23:30

--- GUI/final project/main.py
+++ GUI/final project/main.py
@@ -2,6 +2,28 @@ from tkinter import *
 from tkinter import messagebox
 import menu, setup
 
+def credentials():
+	
+
+def login():
+	Label(root, text="Login", font=("Times New Roman", 25), justify='center', bg=baseColor, fg=txtColor).pack(fill='x')
+
+	userFrame = LabelFrame(root, text="ID", font=("Times New Roman", 15), bg=baseColor, fg=txtColor)
+
+	user = Entry(userFrame, bg=baseColor, fg=txtColor, bd=0, font=("", 15))
+	user.pack(side="left", fill='x', expand=True)
+
+	userFrame.pack(fill='x')
+
+	passFrame = LabelFrame(root, text="Password", font=("Times New Roman", 15), bg=baseColor, fg=txtColor)
+
+	password = Entry(passFrame, bg=baseColor, fg=txtColor, bd=0, font=("", 15), show="•")
+	password.pack(side="left", fill='x', expand=True)
+
+	passFrame.pack(fill='x')
+
+	Button(root, bg=baseColor, fg=txtColor, text="Login", activebackground=baseColor, activeforeground=txtColor, command=lambda: credentials()).pack(fill='x', pady=5)
+
 
 def start():
 	global root, baseColor, txtColor
@@ -13,7 +35,7 @@ def start():
 	txtColor = "#ffffff"
 	root.config(bg=baseColor, padx=5)
 
-	
+	login()
 
 	menu.menuSetup(root)
 
