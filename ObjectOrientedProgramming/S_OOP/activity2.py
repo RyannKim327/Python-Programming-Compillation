0e2a6c3c38f6adc11a45b48aa7daa3135cda12f7
RyannKim327 <rksesgundo123@gmail.com>
2023-10-04 11:00:36 +0800

    undefined 11:00:35 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/activity2.py
+++ ObjectOrientedProgramming/S_OOP/activity2.py
@@ -1,6 +1,10 @@
 class IT2A:
 	def __init__(self, name, section, subject):
-		
+		self.attendance = 0
 		self.name = name
 		self.section = section
 		self.subject = subject
+		msg = f"Student name: {self.name}\nSection: {self.section}\nSubject: {self.subject}"
+		print(msg)
+
+kim = IT2A("Kim")
