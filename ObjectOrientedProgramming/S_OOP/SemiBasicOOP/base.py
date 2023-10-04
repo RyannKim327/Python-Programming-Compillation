d051128776a03bad1615ca52c2b1c0474c2653be
RyannKim327 <rksesgundo123@gmail.com>
2023-10-04 11:15:16 +0800

    undefined 11:15:16 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/SemiBasicOOP/base.py
+++ ObjectOrientedProgramming/S_OOP/SemiBasicOOP/base.py
@@ -1,15 +1,15 @@
 class Animal:
-	def __init__(self, type, sound):
-		self.type = type
-		self.sound = sound
+	def __init__(self):
+		self.type = "Animal"
+		self.sound = "Wala"
 
 	def itis(self):
 		print(f"The {self.type} sounds like {self.sound}")
 
 class Dog(Animal):
-	def __init__(self, type, sound):
+	def __init__(self):
 		self.type = "DOG"
 		self.sound = "Awrf"
 
-dog = Dog("Dog", "Awrf")
+dog = Dog();
 dog.itis()
