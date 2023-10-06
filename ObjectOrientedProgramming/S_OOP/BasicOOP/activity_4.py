56159d53e28a58d058f36b5339ef2309daa10ef9
RyannKim327 <rksesgundo123@gmail.com>
2023-10-06 11:06:02 +0800

    undefined 11:06:02 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/activity_4.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/activity_4.py
@@ -3,5 +3,12 @@ class BankAccount:
 		self.account_holder = account_holder
 		self.initial_deposit = initial_deposit
 
+		while self.initial_deposit < 500:
+			value = 
+			self.setDeposit(value)
+
 	def setDeposit(self, value=0):
-		if value.isdigit()
+		if value.isdigit():
+			return value
+		else:
+			return 0
