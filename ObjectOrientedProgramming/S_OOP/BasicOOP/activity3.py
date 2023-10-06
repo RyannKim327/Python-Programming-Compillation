1d5f593c7c686c332ba8db3fb0960caea2445126
RyannKim327 <rksesgundo123@gmail.com>
2023-10-06 11:18:22 +0800

    undefined 11:18:22 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
@@ -10,11 +10,15 @@ while True:
 	else:
 		val = 0
 	user = BankAccount(name, val)
-	print("""
+	_ = ["1", "2", "3"]
+	a = input("""
 		Select your activity:
 	   [1] Check Balance
-	   [2]
+	   [2] Widraw
+	   [3] Deposit
+		   [4]
 	""")
+	while 
 	users.append(user)
 	if "n" in input("Do you want to continue? ").lower():
 		break
