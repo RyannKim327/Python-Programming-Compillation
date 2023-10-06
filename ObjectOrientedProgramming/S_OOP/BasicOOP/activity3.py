28cd71648a9bd4c58de0c082a997a34832792834
RyannKim327 <rksesgundo123@gmail.com>
2023-10-06 11:14:51 +0800

    undefined 11:14:50 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
@@ -2,11 +2,13 @@ from activity3_external import BankAccount
 
 while True:
 	name = input("Enter account holder: ")
-	val = input("Enter initial deposit")
+	val = input("Enter initial deposit: ")
 	if val.isdigit():
 		val = int(val)
 	else:
 		val = 0
 	user = BankAccount(name, val)
+
+	
 	if "y" in input("Do you want to continue? ").lower():
 		break
