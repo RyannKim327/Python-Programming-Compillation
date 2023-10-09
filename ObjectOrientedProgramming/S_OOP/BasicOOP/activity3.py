96ac8bd83f019edd14b167d828b95bf214cfe8ba
RyannKim327 <rksesgundo123@gmail.com>
2023-10-09 11:08:12 +0800

    undefined 11:08:12 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
@@ -8,7 +8,10 @@ def createAccount():
 	while not balance.isdigit():
 		balance = input("Enter your balance (500 Minimum): ")
 	while int(balance) < 500:
-		
+		balance = input("Enter your balance (500 Minimum): ")
+
+	new_user = BankAccount(name, int(balance))
+
 	a = ["1", "2", "3", "4"]
 	choice = input("Menu:\n[1]Check Balance\n[2]Deposit\n[3]Widraw\nEnter your choice:")
 
