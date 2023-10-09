92b37ed6a1700421c5a265f533b20585a6e34827
RyannKim327 <rksesgundo123@gmail.com>
2023-10-09 10:50:01 +0800

    undefined 10:50:01 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
@@ -3,46 +3,4 @@ from activity3_external import BankAccount
 users = []
 
 while True:
-	name = input("Enter account holder: ")
-	val = input("Enter initial deposit: ")
-	if val.isdigit():
-		val = int(val)
-	else:
-		val = 0
-	user = BankAccount(name, val)
-	_ = ["1", "2", "3", "4"]
-	a = input("""
-Select your activity:
-[1] Check Balance
-[2] Widraw
-[3] Deposit
-[4] Exit
-""")
-	while not a in _:
-		a = input("Enter your choice: ")
-	
-	while a != "4":
-		match(a):
-			case "1":
-				print(f"Your current balance is: {user.checkBalance()}")
-			case "2":
-				b = input("Enter your widrawal: ")
-				user.setWidraw(b)
-			case "3":
-				b = input("Enter your deposit: ")
-				user.setDeposit(b)
-		
-		a = input("""
-1] Check Balance
-2] Widraw
-3] Deposit
-4] Exit
-Enter your choice: """)
-		while not a in _:
-			a = input("Enter your choice: ")
-
-	users.append(user)
-	if "n" in input("Do you want to continue? ").lower():
-		break
-
-print(users[0].checkBalance())
+	choice = input("Menu")
