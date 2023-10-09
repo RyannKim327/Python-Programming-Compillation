7abeeaf9866c4ff94d71a36da76ada32ce914d4a
RyannKim327 <rksesgundo123@gmail.com>
2023-10-09 11:04:41 +0800

    undefined 11:04:41 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/activity3.py
@@ -2,6 +2,9 @@ from activity3_external import BankAccount
 
 users = []
 
+def createAccount():
+	
+
 def start():
 	while True:
 		a = ["1", "2"]
@@ -9,7 +12,9 @@ def start():
 		while not choice in a:
 			choice = input("Menu\n[1] Create account\n[2] Exit")
 		if a == "1":
-			
+			createAccount()
+		else:
+			print("Thank you")
 
 if __name__ == "__main__":
 	start()
