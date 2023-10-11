5e073799d26e984ef72c7ec72db2622e22ab3123
RyannKim327 <rksesgundo123@gmail.com>
2023-10-11 10:46:16 +0800

    undefined 10:46:16 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/activity4_external.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/activity4_external.py
@@ -1,3 +1,11 @@
 class Student:
 	def __init__(self, student: dict):
 		self._student_ = student['studentName']
+
+class Librarian:
+	def __init__(self):
+		pass
+
+class Books:
+	def __init__(self):
+		pass
