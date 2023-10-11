9f8f1ed43b8a9d9e7652ce5888cd2942adf4699c
RyannKim327 <rksesgundo123@gmail.com>
2023-10-11 10:58:01 +0800

    undefined 10:58:01 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1.py.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1.py.py
@@ -8,7 +8,7 @@ def start():
 	_a_ = ["1", "2", "3"]
 
 	while choice in _a_:
-		choice = input("Login as:\n[1] Student\n[2] Librarian\n[3] Exit")
+		choice = input("Login as:\n[1] Student\n[2] Librarian\n[3] Exit\n")
 	
 	match choice:
 		case "1":
