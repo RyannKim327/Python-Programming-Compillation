dd6338a51a8ee565e63b06526acebc9226c675f7
RyannKim327 <rksesgundo123@gmail.com>
2023-10-11 10:58:36 +0800

    undefined 10:58:36 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1.py.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1.py.py
@@ -1,18 +1,18 @@
 from code_challenge1_external import *
 
-student = []
+students= []
 librarian = []
 
 def start():
-	choice = input("Login as:\n[1] Student\n[2] Librarian\n[3] Exit")
+	choice = input("Login as:\n[1] Student\n[2] Librarian\n[3] Exit\nEnter your choice")
 	_a_ = ["1", "2", "3"]
 
-	while choice in _a_:
-		choice = input("Login as:\n[1] Student\n[2] Librarian\n[3] Exit\n")
+	while not choice in _a_:
+		choice = input("Login as:\n[1] Student\n[2] Librarian\n[3] Exit\nEnter your choice")
 	
 	match choice:
 		case "1":
-			id = len(student)
+			id = len(students)
 			name = input("Enter your name: ")
 			student = Student({
 				"ID": id,
