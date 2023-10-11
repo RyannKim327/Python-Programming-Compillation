9bdb21b2c521937cb3c9594c93a0552333130ea2
RyannKim327 <rksesgundo123@gmail.com>
2023-10-11 10:48:02 +0800

    undefined 10:48:02 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1_external.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1_external.py
@@ -3,8 +3,9 @@ class Student:
 		self._id_ = student['ID']
 		self._student_ = student['studentName']
 
+
 class Librarian:
-	def __init__(self):
+	def __init__(self, librarian: dict):
 		pass
 
 class Books:
