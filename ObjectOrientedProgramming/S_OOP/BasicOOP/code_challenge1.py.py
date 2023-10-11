5bb05cdbe07b89b316c5eaeb4f7593e47fa7fa76
RyannKim327 <rksesgundo123@gmail.com>
2023-10-11 10:52:08 +0800

    undefined 10:52:08 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1.py.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1.py.py
@@ -1,3 +1,8 @@
 from code_challenge1_external import *
 
-choice
+choice = input("Login as:\n[1] Student\n[2] Librarian\n[3] Exit")
+_a_ = ["1", "2", "3"]
+
+while choice in _a_:
+	choice = input("Login as:\n[1] Student\n[2] Librarian\n[3] Exit")
+	
