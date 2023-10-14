cf552ebb925fab965f22f4a0e8373745183cca24
RyannKim327 <rksesgundo123@gmail.com>
2023-10-14 12:55:15 +0800

    undefined 12:55:15 PM [autogit]

---
+++ la.py
@@ -0,0 +1,46 @@
+import time
+
+def getTime():
+	timer = time.ctime()
+	time1 = timer.split(" ")[3]
+	return time1.split(":")
+
+def convert(a):
+	if a.isdigit():
+		b = int(a) % 12
+		if b == 0:
+			return 12
+		return b
+	else:
+		return 0
+
+def start():
+	t = [
+		[16, 8, 4, 2, 1],
+		[16, 8, 4, 2, 1],
+		[16, 8, 4, 2, 1]
+	]
+	timer = getTime()
+	a = "*"
+	b = "."
+	hr = 16# convert(timer[0])
+	min = convert(timer[1])
+	sec = convert(timer[2])
+	_hr = ""
+	_min = ""
+	_sec = ""
+	_hr_ = []
+	while hr > 0:
+		print(hr)
+		_hr_.append(hr // 12)
+		hr %= 12
+	# for i in t[0]:
+	# 	if i in _hr_:
+	# 		_hr += a
+	# 	else:
+	# 		_hr += b
+
+	print(_hr)
+	
+if __name__ == "__main__":
+	start()
