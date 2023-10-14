d51b709044edee53d1cbc0759388c4b342f8ccd5
RyannKim327 <rksesgundo123@gmail.com>
2023-10-14 14:50:26 +0800

    undefined 2:50:25 PM [autogit]

--- WebScraping/google.py
+++ WebScraping/google.py
@@ -2,7 +2,10 @@ from bs4 import BeautifulSoup as soup
 import requests as req
 
 def getData(query):
-	a = req.get(f"https://www.google.com/search?q={query}").text()
+	a = req.get(f"https://www.google.com/search?q={query}").text
 	return a
 
-print(getData("sample"))
+def scrape(query):
+	data = getData(query)
+	bs = soup(data, "html.parser")
+	
