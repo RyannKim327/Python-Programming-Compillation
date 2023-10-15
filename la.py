126aeb5755f379fa6b41cb00c57f136871ddf504
RyannKim327 <rksesgundo123@gmail.com>
2023-10-15 10:36:04 +0800

    undefined 10:36:04 AM [autogit]

--- la.py
+++ la.py
@@ -56,68 +56,68 @@ def start():
 	fh = Frame(base)
 
 	h = Label(fh, text="32", bg="red", font=("", 20))
-	h.pack(side='left', fill='both', expand=True)
+	h.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	h1 = Label(fh, text="16", bg="blue", font=("", 20))
-	h1.pack(side='left', fill='both', expand=True)
+	h1.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 	
 	h2 = Label(fh, text="08", bg="red", font=("", 20))
-	h2.pack(side='left', fill='both', expand=True)
+	h2.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	h3 = Label(fh, text="04", bg="red", font=("", 20))
-	h3.pack(side='left', fill='both', expand=True)
+	h3.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	h4 = Label(fh, text="02", bg="red", font=("", 20))
-	h4.pack(side='left', fill='both', expand=True)
+	h4.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	h5 = Label(fh, text="01", bg="red", font=("", 20))
-	h5.pack(side='left', fill='both', expand=True)
+	h5.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
-	fh.pack(fill='both', expand=True)
+	fh.pack(padx=1, pady=1, fill='both', expand=True)
 
 	fm = Frame(base)
 
 	m = Label(fm, text="32", bg="red", font=("", 20))
-	m.pack(side='left', fill='both', expand=True)
+	m.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	m1 = Label(fm, text="16", bg="blue", font=("", 20))
-	m1.pack(side='left', fill='both', expand=True)
+	m1.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 	
 	m2 = Label(fm, text="08", bg="red", font=("", 20))
-	m2.pack(side='left', fill='both', expand=True)
+	m2.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	m3 = Label(fm, text="04", bg="red", font=("", 20))
-	m3.pack(side='left', fill='both', expand=True)
+	m3.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	m4 = Label(fm, text="02", bg="red", font=("", 20))
-	m4.pack(side='left', fill='both', expand=True)
+	m4.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	m5 = Label(fm, text="01", bg="red", font=("", 20))
-	m5.pack(side='left', fill='both', expand=True)
+	m5.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
-	fm.pack(fill='both', expand=True)
+	fm.pack(padx=1, pady=1, fill='both', expand=True)
 
 	fs = Frame(base)
 
 	s = Label(fs, text="32", bg="red", font=("", 20))
-	s.pack(side='left', fill='both', expand=True)
+	s.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	s1 = Label(fs, text="16", bg="blue", font=("", 20))
-	s1.pack(side='left', fill='both', expand=True)
+	s1.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 	
 	s2 = Label(fs, text="08", bg="red", font=("", 20))
-	s2.pack(side='left', fill='both', expand=True)
+	s2.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	s3 = Label(fs, text="04", bg="red", font=("", 20))
-	s3.pack(side='left', fill='both', expand=True)
+	s3.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	s4 = Label(fs, text="02", bg="red", font=("", 20))
-	s4.pack(side='left', fill='both', expand=True)
+	s4.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	s5 = Label(fs, text="01", bg="red", font=("", 20))
-	s5.pack(side='left', fill='both', expand=True)
+	s5.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
-	fs.pack(fill='both', expand=True)
+	fs.pack(padx=1, pady=1, fill='both', expand=True)
 
 	hs = [h, h1, h2, h3, h4, h5]
 	ms = [m, m1, m2, m3, m4, m5]
