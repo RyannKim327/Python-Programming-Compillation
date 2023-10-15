0058f6b268a0d91333c11f1a85e2874c21abe724
RyannKim327 <rksesgundo123@gmail.com>
2023-10-15 10:48:24 +0800

    undefined 10:48:24 AM [autogit]

--- la.py
+++ la.py
@@ -175,24 +175,23 @@ def start2():
 	fm.pack(padx=1, fill='both', expand=True)
 	fs = Frame(base)
 
-	h4 = Label(fh, text="02", bg="red", font=("", 20))
-	h4.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	h5 = Label(fh, text="01", bg="red", font=("", 20))
-	h5.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	
-	m4 = Label(fm, text="02", bg="red", font=("", 20))
-	m4.pack(padx=1, side='left', fill='both', expand=True)
-
-	m5 = Label(fm, text="01", bg="red", font=("", 20))
-	m5.pack(padx=1, side='left', fill='both', expand=True)
-
 	s4 = Label(fs, text="02", bg="red", font=("", 20))
 	s4.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	s5 = Label(fs, text="01", bg="red", font=("", 20))
 	s5.pack(padx=1, pady=1, side='left', fill='both', expand=True)
+	
+	m4 = Label(fs, text="02", bg="red", font=("", 20))
+	m4.pack(padx=1, side='left', fill='both', expand=True)
+
+	m5 = Label(fs, text="01", bg="red", font=("", 20))
+	m5.pack(padx=1, side='left', fill='both', expand=True)
+
+	h4 = Label(fs, text="02", bg="red", font=("", 20))
+	h4.pack(padx=1, pady=1, side='left', fill='both', expand=True)
+
+	h5 = Label(fs, text="01", bg="red", font=("", 20))
+	h5.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
 	fs.pack(padx=1, pady=1, fill='both', expand=True)
 	
