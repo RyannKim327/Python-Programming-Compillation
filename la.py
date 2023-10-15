bb765bf9e82ab54e6b459688e506818787aa39e5
RyannKim327 <rksesgundo123@gmail.com>
2023-10-15 10:21:57 +0800

    undefined 10:21:57 AM [autogit]

--- la.py
+++ la.py
@@ -59,63 +59,63 @@ def start():
 	h.pack(expand=True, fill='x')
 
 	h1 = Label(fh, text="16", font=("", 20), width=5)
-	h1.grid(row=0, column=1)
+	h1.pack(expand=True, fill='x')
 	
 	h2 = Label(fh, text="8", font=("", 20), width=5)
-	h2.grid(row=0, column=2)
+	h2.pack(expand=True, fill='x')
 
 	h3 = Label(fh, text="4", font=("", 20), width=5)
-	h3.grid(row=0, column=3)
+	h3.pack(expand=True, fill='x')
 
 	h4 = Label(fh, text="2", font=("", 20), width=5)
-	h4.grid(row=0, column=4)
+	h4.pack(expand=True, fill='x')
 
 	h5 = Label(fh, text="1", font=("", 20), width=5)
-	h5.grid(row=0, column=5)
+	h5.pack(expand=True, fill='x')
 
 	fh.pack()
 
-	m = Label(base, text="32", font=("", 20), width=5)
-	m.grid(row=1, column=0)
+	# m = Label(base, text="32", font=("", 20), width=5)
+	# m.grid(row=1, column=0)
 
-	m1 = Label(base, text="16", font=("", 20), width=5)
-	m1.grid(row=1, column=1)
+	# m1 = Label(base, text="16", font=("", 20), width=5)
+	# m1.grid(row=1, column=1)
 	
-	m2 = Label(base, text="8", font=("", 20), width=5)
-	m2.grid(row=1, column=2)
+	# m2 = Label(base, text="8", font=("", 20), width=5)
+	# m2.grid(row=1, column=2)
 
-	m3 = Label(base, text="4", font=("", 20), width=5)
-	m3.grid(row=1, column=3)
+	# m3 = Label(base, text="4", font=("", 20), width=5)
+	# m3.grid(row=1, column=3)
 
-	m4 = Label(base, text="2", font=("", 20), width=5)
-	m4.grid(row=1, column=4)
+	# m4 = Label(base, text="2", font=("", 20), width=5)
+	# m4.grid(row=1, column=4)
 
-	m5 = Label(base, text="1", font=("", 20), width=5)
-	m5.grid(row=1, column=5)
+	# m5 = Label(base, text="1", font=("", 20), width=5)
+	# m5.grid(row=1, column=5)
 
-	s = Label(base, text="32", font=("", 20), width=5)
-	s.grid(row=2, column=0)
+	# s = Label(base, text="32", font=("", 20), width=5)
+	# s.grid(row=2, column=0)
 
-	s1 = Label(base, text="16", font=("", 20), width=5)
-	s1.grid(row=2, column=1)
+	# s1 = Label(base, text="16", font=("", 20), width=5)
+	# s1.grid(row=2, column=1)
 	
-	s2 = Label(base, text="8", font=("", 20), width=5)
-	s2.grid(row=2, column=2)
+	# s2 = Label(base, text="8", font=("", 20), width=5)
+	# s2.grid(row=2, column=2)
 
-	s3 = Label(base, text="4", font=("", 20), width=5)
-	s3.grid(row=2, column=3)
+	# s3 = Label(base, text="4", font=("", 20), width=5)
+	# s3.grid(row=2, column=3)
 
-	s4 = Label(base, text="2", font=("", 20), width=5)
-	s4.grid(row=2, column=4)
+	# s4 = Label(base, text="2", font=("", 20), width=5)
+	# s4.grid(row=2, column=4)
 
-	s5 = Label(base, text="1", font=("", 20), width=5)
-	s5.grid(row=2, column=5)
+	# s5 = Label(base, text="1", font=("", 20), width=5)
+	# s5.grid(row=2, column=5)
 
-	hs = [h, h1, h2, h3, h4, h5]
-	ms = [m, m1, m2, m3, m4, m5]
-	ss = [s, s1, s2, s3, s4, s5]
+	# hs = [h, h1, h2, h3, h4, h5]
+	# ms = [m, m1, m2, m3, m4, m5]
+	# ss = [s, s1, s2, s3, s4, s5]
 
-	run()
+	# run()
 
 	base.mainloop()
 
