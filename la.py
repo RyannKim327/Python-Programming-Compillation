452c67500d9726d1ee7f49376dcae58d9c593b53
RyannKim327 <rksesgundo123@gmail.com>
2023-10-15 10:45:28 +0800

    undefined 10:45:27 AM [autogit]

--- la.py
+++ la.py
@@ -139,16 +139,20 @@ def start2():
 	h1 = Label(fh, text="16", bg="blue", font=("", 20))
 	h1.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 	
-	m = Label(fm, text="32", bg="red", font=("", 20))
+	m = Label(fh, text="32", bg="red", font=("", 20))
 	m.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 
-	m1 = Label(fm, text="16", bg="blue", font=("", 20))
+	m1 = Label(fh, text="16", bg="blue", font=("", 20))
 	m1.pack(padx=1, side='left', fill='both', expand=True)
 	
+	s = Label(fh, text="32", bg="red", font=("", 20))
+	s.pack(padx=1, pady=1, side='left', fill='both', expand=True)
+
+	s1 = Label(fh, text="16", bg="blue", font=("", 20))
+	s1.pack(padx=1, pady=1, side='left', fill='both', expand=True)
+
 	h2 = Label(fh, text="08", bg="red", font=("", 20))
 	h2.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-
 
 	h3 = Label(fh, text="04", bg="red", font=("", 20))
 	h3.pack(padx=1, pady=1, side='left', fill='both', expand=True)
@@ -179,12 +183,6 @@ def start2():
 	fm.pack(padx=1, fill='both', expand=True)
 
 	fs = Frame(base)
-
-	s = Label(fs, text="32", bg="red", font=("", 20))
-	s.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	s1 = Label(fs, text="16", bg="blue", font=("", 20))
-	s1.pack(padx=1, pady=1, side='left', fill='both', expand=True)
 	
 	s2 = Label(fs, text="08", bg="red", font=("", 20))
 	s2.pack(padx=1, pady=1, side='left', fill='both', expand=True)
