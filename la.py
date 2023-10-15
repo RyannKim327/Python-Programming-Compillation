a579e8741ce577ed2dd0a8e359e04ddd30a51250
RyannKim327 <rksesgundo123@gmail.com>
2023-10-15 10:51:55 +0800

    undefined 10:51:55 AM [autogit]

--- la.py
+++ la.py
@@ -132,69 +132,6 @@ def start2():
 	base.geometry("500x500")
 
 	fh = Frame(base)
-
-	s = Label(fh, text="32", bg="red", font=("", 20))
-	s.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	s1 = Label(fh, text="16", bg="blue", font=("", 20))
-	s1.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-	
-	m = Label(fh, text="32", bg="red", font=("", 20))
-	m.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	m1 = Label(fh, text="16", bg="blue", font=("", 20))
-	m1.pack(padx=1, side='left', fill='both', expand=True)
-	
-	h = Label(fh, text="32", bg="red", font=("", 20))
-	h.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	h1 = Label(fh, text="16", bg="blue", font=("", 20))
-	h1.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	fh.pack(padx=1, pady=1, fill='both', expand=True)
-	fm = Frame(base)
-
-	m2 = Label(fm, text="08", bg="red", font=("", 20))
-	m2.pack(padx=1, side='left', fill='both', expand=True)
-
-	m3 = Label(fm, text="04", bg="red", font=("", 20))
-	m3.pack(padx=1, side='left', fill='both', expand=True)
-
-	h4 = Label(fm, text="02", bg="red", font=("", 20))
-	h4.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	h3 = Label(fm, text="04", bg="red", font=("", 20))
-	h3.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	s3 = Label(fm, text="04", bg="red", font=("", 20))
-	s3.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	s4 = Label(fm, text="02", bg="red", font=("", 20))
-	s4.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	fm.pack(padx=1, fill='both', expand=True)
-	fs = Frame(base)
-	
-	m4 = Label(fs, text="02", bg="red", font=("", 20))
-	m4.pack(padx=1, side='left', fill='both', expand=True)
-
-	m5 = Label(fs, text="01", bg="red", font=("", 20))
-	m5.pack(padx=1, side='left', fill='both', expand=True)
-
-	h2 = Label(fs, text="08", bg="red", font=("", 20))
-	h2.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	h5 = Label(fs, text="01", bg="red", font=("", 20))
-	h5.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	s2 = Label(fm, text="08", bg="red", font=("", 20))
-	s2.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	s5 = Label(fm, text="01", bg="red", font=("", 20))
-	s5.pack(padx=1, pady=1, side='left', fill='both', expand=True)
-
-	fs.pack(padx=1, pady=1, fill='both', expand=True)
-
 	base.mainloop()
 	
 if __name__ == "__main__":
