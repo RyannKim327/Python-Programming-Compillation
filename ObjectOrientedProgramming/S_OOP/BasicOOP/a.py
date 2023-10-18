f516da27cb03cdf57359a02cfd365b2053eef906
RyannKim327 <rksesgundo123@gmail.com>
2023-10-18 09:52:11 +0800

    undefined 9:52:10 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/a.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/a.py
@@ -1,10 +1,10 @@
 class class_s:
 	def __init__(self, a: str, b):
-		"""Sample"""
+		"""Sample program 123"""
 		print("Initated")
 
 	def __str__(self):
 		return "This class is for something"
 
-s = 
+s = class_s()
 print(s)
