3cd1d4f2441b836928eba52d97eb6531d31a1f53
RyannKim327 <rksesgundo123@gmail.com>
2023-10-20 07:59:37 +0800

    undefined 7:59:37 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/a.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/a.py
@@ -12,7 +12,12 @@ class Book:
 
 
 class Librarian:
-	
+	def __init__(self, ID, name, author, pub, stocks=0):
+		self.ID = ID
+		self.name = name
+		self.author = author
+		self.pub = pub
+		self.stocks = stocks
 
 students = []
 librarian = []
