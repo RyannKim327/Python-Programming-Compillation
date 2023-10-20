59abbf3e654affdff597175d0c922ccee0a0f8d6
RyannKim327 <rksesgundo123@gmail.com>
2023-10-20 08:06:39 +0800

    undefined 8:06:39 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/code_challenge1.py
@@ -66,7 +66,7 @@ def start():
 			else:
 				books_ = [f"{i.getBookName()}\n- Stocks: [{i.getStocks()}]" for i in books]
 				for i in books:
-					books_.append()
+					books_.append(f"{i.getBookName()}\n- Stocks: [{i.getStocks()}]")
 				another_choice = insertlists(books_, "Enter a book you want to borrow: ")
 				while not another_choice.isdigit():
 					another_choice = input("Enter a book you want to borrow: ")
