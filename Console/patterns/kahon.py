2f4881b8b9b5b56443bb707687d43062d2bc17e0
RyannKim327 <rksesgundo123@gmail.com>
2023-10-24 05:09:51 +0800

    undefined 5:09:51 AM [autogit]

--- Console/patterns/kahon.py
+++ Console/patterns/kahon.py
@@ -1,13 +1,14 @@
 n  = int(input("Enter numbers of squares: "))
 r = ""
-
+x =  True
 for i in range(n):
 	for j in range(n):
 		for k in range(n):
-			if i == 0 or k == 0 or k == n-1 or i == n-1:
+			if i == 0 or k == 0 or k == n-1 or i == n-1 or x:
 				r += "*"
 			else:
 				r += " "
+			if k 
 	r += "\n"
 
 print(r)
