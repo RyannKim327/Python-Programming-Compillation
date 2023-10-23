2d72f15b0a780aa022516dfcc5586724dc977713
RyannKim327 <rksesgundo123@gmail.com>
2023-10-24 05:15:53 +0800

    undefined 5:15:53 AM [autogit]

--- Console/patterns/kahon.py
+++ Console/patterns/kahon.py
@@ -3,7 +3,9 @@ r = ""
 
 for i in range(n):
 	for j in range(n):
-		if i == 0 or i == n-1 or 
+		if i == 0 or i == n-1 or j == 0 or j == n-1:
+			r += "* "
+		else:
 		for k in range(n):
 			r += "* "
 		for k in range(n-2):
