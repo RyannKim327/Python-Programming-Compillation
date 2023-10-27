3094351aae7551e65e374449b1e5ddbfc10ffcfb
RyannKim327 <rksesgundo123@gmail.com>
2023-10-27 16:12:42 +0800

    undefined 4:12:42 PM [autogit]

--- ObjectOrientedProgramming/S_OOP/BasicOOP/error.py
+++ ObjectOrientedProgramming/S_OOP/BasicOOP/error.py
@@ -1,12 +1,12 @@
 class Error:
 	def __init__(self):
-		self.errors = ()
+		self.errors = []
 	
 	def getAllErrors(self):
 		for i in self.errors:
 			print(i)
 
-	def addError(self, error: str):
+	def addError(self, *error: str):
 		self.errors.append(error)
 
 error = input("Enter a sample error: ")
