272306451f291abfb9ced0e92c58682c60235923
RyannKim327 <rksesgundo123@gmail.com>
2023-11-13 11:21:01 +0800

    undefined 11:21:01 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/Midterms/carpool.py
+++ ObjectOrientedProgramming/S_OOP/Midterms/carpool.py
@@ -1,5 +1,7 @@
 class Vehicle:
-	def __init__(self, name: str, capacity = 3, driver: Driver, fare: float)
+	def __init__(self, name: str, capacity = 3, driver: Driver, fare: float):
+		self.name = name
+		
 
 class Driver:
 	def __init__(self):
