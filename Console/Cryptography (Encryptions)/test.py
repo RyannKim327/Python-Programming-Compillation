57caf38b2c88de21bc4d269d968f1ef2339b40eb
RyannKim327 <rksesgundo123@gmail.com>
2023-11-17 04:28:10 +0800

    undefined 4:28:10 AM [autogit]

--- Console/Cryptography (Encryptions)/test.py
+++ Console/Cryptography (Encryptions)/test.py
@@ -5,15 +5,18 @@ for i in range(1, 7):
 		table.append(i * j)
 
 n = input("Enter string: ")
-o = ""
 def encrypt(n):
+	o = ""
 	for i in range(len(n)):
 		m = chr(ord(n[i]) + table[i % 26])
 		o += m
+	return o
 
 def decrypt(n):
+	o =  ""
 	for i in range(len(n)):
 		m = chr(ord(n[i]) - table[i % 26])
 		o += m
+	return o
 
-print(decrypt(o))
+print(decrypt(n))
