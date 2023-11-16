0c804f6e40f14df93609b8a91647c72efb453f9c
RyannKim327 <rksesgundo123@gmail.com>
2023-11-17 04:25:49 +0800

    undefined 4:25:49 AM [autogit]

--- Console/Cryptography (Encryptions)/test.py
+++ Console/Cryptography (Encryptions)/test.py
@@ -1,8 +1,10 @@
-
 table = []
 
-for i in range(1, 6):
-	for j in range(1, 6):
+for i in range(1, 7):
+	for j in range(1, 7):
 		table.append(i * j)
 
-print(len(table))
+n = input("Enter string: ")
+
+for i in range(n):
+	m = ord(n[i]) + 
