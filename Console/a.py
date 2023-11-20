5b14d74947985de6ac691e643984f4ed5f1035eb
RyannKim327 <rksesgundo123@gmail.com>
2023-11-21 07:57:23 +0800

    undefined 7:57:23 AM [autogit]

--- Console/a.py
+++ Console/a.py
@@ -2,5 +2,10 @@ students_data = [
 	["Alice", [85, 90, 92], ["Math", "English", "Science"]],
 	["Bob", [78, 88, 80], ["Math", "English", "Science"]],
 	["Charlie", [92, 94, 89], ["Math", "English", "Science"]],
-["David", [80, 85, 88], ["Math", "English", "Science"]],
+	["David", [80, 85, 88], ["Math", "English", "Science"]],
 ]
+
+student = []
+
+for i in students_data:
+	
