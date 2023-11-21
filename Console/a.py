ff9cdbe305e25d3ad4f4e065d56b039ea33567d1
RyannKim327 <rksesgundo123@gmail.com>
2023-11-21 09:09:56 +0800

    undefined 9:09:56 AM [autogit]

--- Console/a.py
+++ Console/a.py
@@ -6,7 +6,7 @@ students_data = [
 ]
 
 def getStudents(arr: list):
-	student = arr
+	student = []
 
 	# Traversal
 	for i in arr:
@@ -17,47 +17,36 @@ def getStudents(arr: list):
 		ave *= 100
 		ave //= 1
 		ave /= 100
-		student.append(ave)
+		student.append([f"Name: {i[0]}", ave])
 
 	# Sort
 	for i in range(len(student)):
 		for j in range(len(student)):
-			if student[i][-1] < student[j][-1]:
+			if student[i][1] < student[j][1]:
 				student[i], student[j] = student[j], student[i]
 
 	return student
 
-students_data = getStudents(students_data)
-print(f"Sorted by Average: {students_data}")
-
-print()
+new_students = getStudents(students_data)
+print(f"Sorted by Average: {new_students}")
 
 students_data.append(["Eva", [95, 91, 88], ["Math", "English", "Science"]])
 print(f"Added Eva: {students_data}")
 
-print()
-
-students_data = getStudents(students_data)
-print(f"New sort with Eva: {students_data}")
-
-print()
-
-students_data.pop(0)
-print(f"Remove lowest average: {students_data}")
-
-print()
+new_students = getStudents(students_data)
+print(f"New sort with Eva: {new_students}")
+new_students.pop(0)
+print(f"Remove lowest average: {new_students}")
 
 english_scores = []
 for i in range(len(students_data)):
 	english_scores.append(students_data[i][1][1])
 print(english_scores)
 
-print()
-
 maths = [i for i in students_data]
 for i in range(len(maths)):
 	for j in range(len(maths)):
-		if maths[i] > maths[j]:
+		if maths[i] < maths[j]:
 			maths[i], maths[j] = maths[j], maths[i]
 
-print(maths)
+print(maths.rev)
