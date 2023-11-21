4a6182679cd03ad72f97ea4587b020f6da5b0bd1
RyannKim327 <rksesgundo123@gmail.com>
2023-11-21 09:12:37 +0800

    undefined 9:12:37 AM [autogit]

--- Console/a.py
+++ Console/a.py
@@ -17,7 +17,7 @@ def getStudents(arr: list):
 		ave *= 100
 		ave //= 1
 		ave /= 100
-		student.append([f"Name: {i[0]}", ave])
+		.append([f"Name: {i[0]}", ave])
 
 	# Sort
 	for i in range(len(student)):
