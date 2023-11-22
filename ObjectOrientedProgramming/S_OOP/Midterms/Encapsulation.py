f576bd2166ac5b3d958e558602f997696ac1fd25
RyannKim327 <rksesgundo123@gmail.com>
2023-11-23 07:53:16 +0800

    undefined 7:53:15 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/Midterms/Encapsulation.py
+++ ObjectOrientedProgramming/S_OOP/Midterms/Encapsulation.py
@@ -2,6 +2,8 @@ class Payment:
 	def __init__(self, price):
 		self.final_price = price + (price * 0.05)
 		self.__final_price = price + (price * 0.05)
+	
+	def getFinalPrice()
 
 pay = Payment(100)
 print(pay.final_price)
