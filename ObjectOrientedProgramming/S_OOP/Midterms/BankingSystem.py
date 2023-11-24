6314447fa08a0be9f15c286437b5350aa9423037
RyannKim327 <rksesgundo123@gmail.com>
2023-11-24 10:58:41 +0800

    undefined 10:58:41 AM [autogit]

--- ObjectOrientedProgramming/S_OOP/Midterms/BankingSystem.py
+++ ObjectOrientedProgramming/S_OOP/Midterms/BankingSystem.py
@@ -1,2 +1,4 @@
 class BackAccount:
-	def __init__(self, accountHolder: str, ):
+	def __init__(self, accountHolder: str, balance: int):
+		self.accountHolder = accountHolder
+		self.balance = balance
