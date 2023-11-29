6a6687131092cee45c79b1abd3e8f44efa39da61
RyannKim327 <rksesgundo123@gmail.com>
2023-11-29 10:47:57 +0800

    undefined 10:47:57 AM [autogit]

---
+++ ObjectOrientedProgramming/S_OOP/Midterms/Inheritance_OOP/base.py
@@ -0,0 +1,16 @@
+class Animal:
+	def __init__(self):
+		self.type = "Animal"
+		self.sound = "Wala"
+
+	def itis(self):
+		print(f"The {self.type} sounds like {self.sound}")
+
+class Dog(Animal):
+	def __init__(self):
+		super().__init__()
+		self.type = "DOG"
+		self.sound = "Awrf"
+	
+dog = Dog()
+dog.itis()
