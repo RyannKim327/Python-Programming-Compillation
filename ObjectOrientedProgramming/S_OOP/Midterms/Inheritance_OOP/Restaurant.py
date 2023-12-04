514f4ad6dda534307984fca5db14e1445667f8d8
RyannKim327 <rksesgundo123@gmail.com>
2023-12-04 21:02:58 +0800

    12/4/2023 9:02:58 PM [autogit]

--- ObjectOrientedProgramming/S_OOP/Midterms/Inheritance_OOP/Restaurant.py
+++ ObjectOrientedProgramming/S_OOP/Midterms/Inheritance_OOP/Restaurant.py
@@ -53,4 +53,7 @@ class Order:
 
 order = Order()
 while True:
-	a = input("Menu: \n[1] Food\n\nEnter your choice:")
+	a = input("Menu: \n[1] Food\n[2] Drinks\nEnter your choice:")
+	if a == 1:
+		pass
+	elif 
