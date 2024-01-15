ffd4f828c918da118783a98aaf79f7b85cd83e60
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 05:47:19 +0800

    01-16-24 05:47

--- Console/games/snake.py
+++ Console/games/snake.py
@@ -64,69 +64,72 @@ def condition(score):
 			penalty = "bitten by the snake"
 	return [score, penalty]
 
-player1 = 0
-player2 = 0
-x = 0
+players = []
+playerscore = []
+player = input("Enter player: ")
+while player != "" or len(players) < 2:
+	if(player != ""):
+		players.append(player)
+		playerscore.append(0)
+	player = input("Enter player: ")
+
 good = [1, 4 ,8, 21, 28, 50, 71, 80]
 landgood = [38, 14, 30, 42, 76, 67, 92, 99]
 bad = [32, 36, 48, 62, 88, 95, 97]
 landbad = [10, 6, 26, 18, 24, 56, 78]
 winner = ""
-while True:
-	board = createBoard()
-	textboard = ""
-	for i in range(len(board)):
-		for j in range(len(board[i])):
-			if board[i][j] in good:
-				textboard += "("
-			elif board[i][j] in bad:
-				textboard += "{"
-			elif board[i][j] in landgood or board[i][j] in landbad:
-				textboard += "["
-			else:
-				textboard += "|"
-			if board[i][j] == player1:
-				textboard +=  "*"
-			elif board[i][j] == player2:
-				textboard +=  "-"
-			else:
-				textboard +=  " "
-			if board[i][j] in good:
-				textboard += ") "
-			elif board[i][j] in bad:
-				textboard += "} "
-			elif board[i][j] in landgood or board[i][j] in landbad:
-				textboard += "] "
-			else:
-				textboard += "| "
-		textboard += "\n"
-	print(textboard, end="\r")
-	input("Enter to continue: ")
-	dice = random.randint(1, 6)
-	if x % 2 == 0:
-		player1 += dice
-	else:
-		player2 += dice
-	if player1 > 100:
-		p = player1 - 100
-		player1 = 100 - p
-	if player2 > 100:
-		p = player2 - 100
-		player2 = 100 - p
-	cond1 = condition(player1)
-	cond2 = condition(player2)
-	player1 = cond1[0]
-	player2 = cond2[0]
-	os.system("cls")
-	if cond1[1] != "" or cond2[1] != "":
-		print(f"Player {(x % 2) + 1} {cond1[1]}{cond2[1]}")
-	print(f"Scoreboard:\nPlayer 1: {player1}\nPlayer 2: {player2}\n\nPlayer {(x % 2) + 1} moved [{dice}] squares")
-	x += 1
-	if player1 == 100:
-		winner = "Player 1"
-		break
-	if player2 == 100:
-		winner = "Player 2"
-		break
+gaming = True
+while gaming:
+	for p in range(len(players)):
+		player = players[p]
+		board = createBoard()
+		textboard = ""
+		for i in range(len(board)):
+			for j in range(len(board[i])):
+				if board[i][j] in good:
+					textboard += "("
+				elif board[i][j] in bad:
+					textboard += "{"
+				elif board[i][j] in landgood or board[i][j] in landbad:
+					textboard += "["
+				else:
+					textboard += "|"
+				for p2 in range(len(players)):
+					if board[i][j] == playerscore[p2]:
+						textboard +=  players[p2][0]
+					else:
+						textboard +=  " "
+				if board[i][j] in good:
+					textboard += ") "
+				elif board[i][j] in bad:
+					textboard += "} "
+				elif board[i][j] in landgood or board[i][j] in landbad:
+					textboard += "] "
+				else:
+					textboard += "| "
+			textboard += "\n"
+		print(textboard, end="\r")
+		input("Enter to continue: ")
+		dice = random.randint(1, 6)
+
+		playerscore[p] += dice
+		
+		if playerscore[p] > 100:
+			p3 = playerscore[p] - 100
+			playerscore[p] = 100 - p3
+		cond1 = condition(playerscore[p])
+		playerscore[p] = cond1[0]
+		os.system("cls")
+		if cond1[1] != "":
+			print(f"Player {player} {cond1[1]}")
+		print("Scoreboard: ")
+		for p4 in range(len(players)):
+			print(f"{players[p4]}: {playerscore[p4]}")
+		print(f"\nPlayer {player} moved [{dice}] squares")
+		
+		if playerscore[p] == 100:
+			winner = player
+			gaming = False
+			break
 
 print(f"The winner is {winner}")
