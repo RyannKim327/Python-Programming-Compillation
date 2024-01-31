ba871b27258640cccd4ef590a8fe33974490e050
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 15:05:05 +0800

    01-31-24 15:05

--- ObjectOrientedProgramming/School/Project/handler.py
+++ ObjectOrientedProgramming/School/Project/handler.py
@@ -1,5 +1,5 @@
 import json
-
+import hashlib
 class Database:
 	def __init__(self):
 		try:
@@ -24,11 +24,38 @@ class UserNotFound(Exception):
 		if name == "":
 			self.__error__ = "The name is blank"
 		elif data.getData().get(name) == None:
-			self.__error__ = "User Not Found"
+			data[name] = {}
+			data.saveData(data)
+			self.__error__ = "User Not Found, the system created the user automatically."
 	
 	def __str__(self):
 		return self.__error__
 
 class UserAuthentication:
 	def __init__(self):
-		self.name = input("Please enter your name: ")
+		self.__authenticate__ = False
+		try:
+			self.name = input("Please enter your name: ")
+			self.password = input("Please enter your password: ")
+			self.__authenticate__ = True
+		except UserNotFound as e:
+			print(e)
+
+class Security:
+	def __init__(self, password: str):
+		self.__password = password
+	
+	def encrypt(self, data: str):
+		result = ""
+		password = self.__password
+		for i in range(len(data)):
+			result += str(ord(data[i]) + ord(password[i % len(password)])) + " "
+		return result
+
+	def decrypt(self, data: str):
+		result = ""
+		d = data.split(" ")
+		password = self.__password
+		for i in range(len(d)):
+			result += chr(int(d[i]) - ord(password[i % len(password)]))
+		return result
--- ObjectOrientedProgramming/School/Project/main.py
+++ ObjectOrientedProgramming/School/Project/main.py
@@ -1,2 +1,7 @@
 from handler import *
+import hashlib
 
+security = Security("RySes")
+
+print(security.encrypt("Hello World"))
+print(security.decrypt("154 222 191 209 226 114 208 194 215 223 182"))
